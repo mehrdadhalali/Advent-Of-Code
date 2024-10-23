13987e7709c9281ff7c0acfed51a6ceb44b8f3ed
mehrdadhalali <trainee.mehrdad.halali@sigmalabs.co.uk>
2024-10-23 14:59:30 +0100

    Completed day 12 challenge 2.

--- 2021/day_12.py
+++ 2021/day_12.py
@@ -14,14 +14,6 @@ class Cave(Node):
     def __repr__(self) -> str:
         return self.name
 
-    def copy(self):
-        """Creates a deep copy of the cave."""
-
-        duplicate = Cave(self.name)
-        duplicate.neighbours = self.neighbours.copy()
-
-        return duplicate
-
 
 def format_data(data: list[str]) -> list[list[str]]:
     """Separates the cave names."""
@@ -58,7 +50,8 @@ def connect_caves(caves: list[Cave], name_pairs: list[list[str]]):
         cave_2.neighbours.add(cave_1)
 
 
-def find_paths(path: list[Cave], current_cave: Cave) -> list[list[str]]:
+def find_paths(path: list[Cave], current_cave: Cave,
+               revisit: Cave = None) -> list[list[str]]:
     """Find the number of paths from this cave to the end."""
 
     path_so_far = path.copy()
@@ -73,9 +66,44 @@ def find_paths(path: list[Cave], current_cave: Cave) -> list[list[str]]:
                   if neighbour.is_big
                   or neighbour not in path_so_far]
 
+    if (revisit is not None
+        and path_so_far.count(revisit) == 1
+            and revisit in current_cave.neighbours):
+        explorable.append(revisit)
+
     return ([path
              for cave in explorable
-             for path in find_paths(path_so_far, cave)])
+             for path in find_paths(path_so_far, cave, revisit)])
+
+
+def get_first_star(caves: list[Cave]) -> int:
+    """Does the first task."""
+
+    return len(find_paths([], find_cave_by_name("start", caves)))
+
+
+def turn_path_to_string(path: list[Cave]) -> str:
+    """Turn a list of caves into something hashable, so it can go in a set."""
+
+    return "-".join([cave.name for cave in path])
+
+
+def get_second_star(caves: list[Cave]) -> int:
+    """Does the second task."""
+
+    revisitable_caves = [cave for cave in caves
+                         if not cave.is_big
+                         and cave.name not in ["start", "end"]]
+
+    paths_per_cave = [find_paths([], find_cave_by_name("start", caves),
+                                 revisit=cave)
+                      for cave in revisitable_caves]
+    all_paths = [path for paths in paths_per_cave
+                 for path in paths]
+
+    all_path_strings = [turn_path_to_string(path) for path in all_paths]
+
+    return len(set(all_path_strings))
 
 
 if __name__ == "__main__":
@@ -83,8 +111,9 @@ if __name__ == "__main__":
     input_data = get_input_data(12, test=False)
     paired_data = format_data(input_data)
 
-    caves = create_caves(paired_data)
+    cave_system = create_caves(paired_data)
 
-    connect_caves(caves, paired_data)
+    connect_caves(cave_system, paired_data)
 
-    print(len(find_paths([], find_cave_by_name("start", caves))))
+    print(get_first_star(cave_system))
+    print(get_second_star(cave_system))
