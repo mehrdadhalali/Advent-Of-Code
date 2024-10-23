b15761716d5d4061f59c2b6bbcc3cdf52d171004
mehrdadhalali <trainee.mehrdad.halali@sigmalabs.co.uk>
2024-10-23 14:14:13 +0100

    Improved the code so it shows the individual paths as well.

--- 2021/day_12.py
+++ 2021/day_12.py
@@ -4,17 +4,23 @@ from helper_classes import Node
 
 
 class Cave(Node):
-    """A cave is like a node in a graph, but it can be explored,
-        it also has a size."""
+    """A cave is like a node in a graph, but it has a size."""
 
     def __init__(self, name):
 
         super().__init__(name)
         self.is_big = self.name.isupper()
-        self.explored = False
 
     def __repr__(self) -> str:
         return self.name
+
+    def copy(self):
+        """Creates a deep copy of the cave."""
+
+        duplicate = Cave(self.name)
+        duplicate.neighbours = self.neighbours.copy()
+
+        return duplicate
 
 
 def format_data(data: list[str]) -> list[list[str]]:
@@ -52,11 +58,33 @@ def connect_caves(caves: list[Cave], name_pairs: list[list[str]]):
         cave_2.neighbours.add(cave_1)
 
 
+def find_paths(path: list[Cave], current_cave: Cave) -> list[list[str]]:
+    """Find the number of paths from this cave to the end."""
+
+    path_so_far = path.copy()
+
+    if current_cave.name == "end":
+        return [path + [current_cave]]
+
+    path_so_far.append(current_cave)
+
+    explorable = [neighbour
+                  for neighbour in current_cave.neighbours
+                  if neighbour.is_big
+                  or neighbour not in path_so_far]
+
+    return ([path
+             for cave in explorable
+             for path in find_paths(path_so_far, cave)])
+
+
 if __name__ == "__main__":
 
-    input_data = get_input_data(12, test=True)
+    input_data = get_input_data(12, test=False)
     paired_data = format_data(input_data)
 
     caves = create_caves(paired_data)
 
     connect_caves(caves, paired_data)
+
+    print(len(find_paths([], find_cave_by_name("start", caves))))
